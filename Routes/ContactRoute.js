a5a5e86201b80c20f65d8da704defdb5c4832691
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-11-16 16:52:37 +0530

    Change for error message

--- Routes/ContactRoute.js
+++ Routes/ContactRoute.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../Models/ContactSchema')
+const bodyParser = require('body-parser');
+const Contact = require('../Models/ContactSchema');
+
+router.use(bodyParser.json());
 
 router.post('/contact', async (req, res) => {
     try {
-
         const contactData = req.body;
         const result = await Contact.create(contactData);
-
-        res.status(201).json({ message: "Data Uploaded Successfully", data: result })
+        res.status(201).json({ message: "Data Uploaded Successfully", data: result });
     } catch (error) {
-
-
-        console.error(error);
-        res.status(500).json(error)
+        console.error("Error creating contact:", error);
+        res.status(500).json({ error: "Internal Server Error", details: error.message || error });
     }
-})
+});
 
 module.exports = router;
